20c7de8aa7158ee341f2bc26ecd14cc25da90cda
evan <mcclanahan@gmail.com>
2012-10-08 13:45:40 -0700

    add some error checking for rpc

--- src/check_msg_q.erl
+++ src/check_msg_q.erl
@@ -21,17 +21,25 @@ option_spec_list() ->
 run_cmd(Options, CmdOptions) ->
     Node = proplists:get_value(node, Options),
     Pids = rpc:call(Node, erlang, processes, []),
-    QTups = [{Pid0, rpc:call(Node, erlang, process_info, [Pid0, message_queue_len])} ||
-                Pid0 <- Pids],
-    MsgCounts = [{Pid, QLen} || {Pid, {message_queue_len, QLen}} <- QTups], 
-    Highest = lists:foldl(fun({_Pid, X},Y) -> 
-                                  case X > Y of 
-                                      true  -> X; 
-                                      false -> Y 
-                                  end 
-                          end, 0, MsgCounts),
+    case is_list(Pids) of
+        true ->
+            QTups = [{Pid0, rpc:call(Node, erlang, 
+                                     process_info, 
+                                     [Pid0, message_queue_len])} ||
+                        Pid0 <- Pids],
+            MsgCounts = [{Pid, QLen} || {Pid, {message_queue_len, QLen}} 
+                                            <- QTups], 
+            Highest = lists:foldl(fun({_Pid, X},Y) -> 
+                                          case X > Y of 
+                                              true  -> X; 
+                                              false -> Y 
+                                          end 
+                                  end, 0, MsgCounts),
 
-    handle_output(0, Highest, CmdOptions).
+            handle_output(0, Highest, CmdOptions);
+        false -> 
+            {error, "process gathering failed"}
+    end.
 
 handle_output(0, Count, CmdOptions) ->
     Critical = proplists:get_value(critical, CmdOptions),
